9f5073a06e8334a53b028f35e0a1d57e95a39903
Natalya <holodilina.n@gmail.com>
2022-09-13 18:25:10 +0300

    открытие для просмотра

---
+++ ProgNumerComparison/Program.cs
@@ -0,0 +1,27 @@
+﻿// Программа, которая на вход принимает два числе и выдает, какое число больше, а какое меньше.
+
+{
+    Console.WriteLine("Введите два числа: ");
+    int a, b;
+    Console.Write("Введите первое число: ");
+    a = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите второе число: ");
+    b = Convert.ToInt32(Console.ReadLine());
+
+    bool Chto0 = a > b;
+    bool Chto1 = a == b;
+    bool Chto2 = a < b;
+
+    if (Chto0)
+    {
+        Console.WriteLine($"Число {a} больше, чем {b}");
+    }
+    else if (Chto1)
+    {
+        Console.WriteLine($"Число {a} и {b} равны");
+    }
+    else
+    {
+        Console.WriteLine($"Число {b} больше, чем {a}");
+    }
+}
