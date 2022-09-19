429912da0fa3837edc2d292086e3be69f8100267
Natalya <holodilina.n@gmail.com>
2022-09-19 15:06:55 +0300

    продолжение занятия

--- Sem1Task3.1/Program.cs
+++ Sem1Task3.1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// #3.1 Второй вариант: Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+
+string? inputLine = Console.ReadLine();
+
+if(inputLine != null)
+{
+    int inputDayOfWeek = int.Parse(inputLine);
+
+string outDayOfWeek = string.Empty; // ""
+    switch(inputNumber)
+    {
+        case 1: outDayOfWeek = "понедельник"; break;
+        case 2: outDayOfWeek = "вторник"; break;
+        case 3: outDayOfWeek = "среда"; break;
+        case 4: outDayOfWeek = "четверг"; break;
+        case 5: outDayOfWeek = "пятница"; break;
+        case 6: outDayOfWeek = "суббота"; break;
+        case 7: outDayOfWeek = "воскресенье"; break;
+
+        default: outDayOfWeek = "Такого дня нет.";
+    }
+}
--- Sem1Task3/Program.cs
+++ Sem1Task3/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// #3 Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+
+string? inputLine = Console.ReadLine();
+
+if(inputLine != null)
+{
+    int inputDayOfWeek = int.Parse(inputLine);
+
+    string[] dayOfWeek = new string[7];
+    dayOfWeek[0] = "Понедельник";
+    dayOfWeek[1] = "Вторник";
+    dayOfWeek[2] = "Среда";
+    dayOfWeek[3] = "Четверг";
+    dayOfWeek[4] = "Пятница";
+    dayOfWeek[5] = "Суббота";
+    dayOfWeek[6] = "Воскресенье";
+
+    if(inputDayOfWeek > 7 || inputDayOfWeek < 1)
+    {
+        Console.WriteLine("Такого дня нет.");
+    }
+    
+    else
+    {
+    Console.WriteLine(dayOfWeek[inputDayOfWeek-1]);
+    }
+}
