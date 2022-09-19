689bb706007339aa8c04a08d49276f60fbc6f423
Natalya <holodilina.n@gmail.com>
2022-09-19 14:30:19 +0300

    Sem1Task1

---
+++ Sem1Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Программа, которая принимает на вход два числа
+// и проверяет является ли первое квадратом второго
+
+string? inputLineA = Console.ReadLine();
+string? inputLineB = Console.ReadLine();
+
+if(inputLineA != null && inputLineB != null)
+{
+    int inputNumberA = int.Parse(inputLineA);
+    int inputNumberB = int.Parse(inputLineB);
+
+    // bool outResult = (inputNumberA * inputNumberA == inputNumberB);
+    // bool outResult = (inputNumberB/inputNumberA == inputNumberA);
+    bool outResult = (Math.Sqrt(inputNumberB) == inputNumberA);
+
+    Console.WriteLine(outResult);
+}
+
