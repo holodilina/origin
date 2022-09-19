92414fff23a87e5ccff2d412daad02feb48ad202
Natalya <holodilina.n@gmail.com>
2022-09-19 16:36:18 +0300

    продолжение заданий семинара 1

---
+++ Sem1Task7/Program.cs
@@ -0,0 +1,14 @@
+﻿// #7 Программа, которая на вход принимает трёхзначное число 
+// и на выходе показывает последнюю цифру этого числа.
+
+string? inputLine = Console.ReadLine();
+
+if (inputLine != null)
+{
+    int inputNumber = int.Parse(inputLine);
+    if(inputNumber > 99 && inputNumber < 1000)
+    {
+        int lastDigit = inputNumber % 10;
+        Console.WriteLine(lastDigit);
+    }
+}
