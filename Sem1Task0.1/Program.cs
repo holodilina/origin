dfb8d53ae24cdfe0744d6c073a831ce3835fa9d5
Natalya <holodilina.n@gmail.com>
2022-09-19 14:29:18 +0300

    Sem1Task0.1

---
+++ Sem1Task0.1/Program.cs
@@ -0,0 +1,20 @@
+﻿// продолжение Task0
+// проверка времени выполнения действия программы
+
+string? inputLine = Console.ReadLine();
+if (inputLine != null)
+{
+    int inputNumber = int.Parse(inputLine);
+    int outNumber = 0;
+
+    DateTime d1 = DateTime.Now;
+    for (int i = 0; i < 10000000; i++)
+    {
+        outNumber = inputNumber * inputNumber;
+    }
+
+    DateTime d2 = DateTime.Now;
+    Console.WriteLine(d2 - d1);
+    
+    Console.WriteLine(outNumber);
+}
